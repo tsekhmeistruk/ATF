6b7defa0da5f3604fd2cad23d3d186df0be45542
tsekhmeistruk <autocrossauto@gmail.com>
2018-01-28 14:17:08 +0200

    initial commit

---
+++ Tests/NavigationMenuTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using PageObjects;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NavigationMenuTest : BaseTest
+    {
+        [Test]
+        public void CheckTitles()
+        {
+            List<string> menuOptions = new List<string>()
+            {
+                "News",
+                "Documentation",
+                "Contact"
+            };
+            var homePage = PageObjectFactory.GetPage<NUnitHomePage>();
+            homePage.Open();
+
+            foreach (var menuOption in menuOptions)
+            {
+                homePage.OpenMenu(menuOption.ToLower());
+                string actualResult = homePage.GetTitle();
+                Assert.IsTrue(actualResult.Contains(menuOption));
+            }        
+        }
+    }
+}
